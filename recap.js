aa8a429127df6dbd5d131d2e305157388cd25e6c
isalim079 <salimreza1025@gmail.com>
2023-08-03 09:01:30 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,4 @@
+var myName = 'Jhambul'   /* This is a variable */
+
+var friends = ['Milu', 'Chilu', 'Bilu', 'Ghilu', 'Malu', 'Kalu']  
+/* friends is an array which contains in third bracket. I use name which is strings. So, I have to put them in quotes */
